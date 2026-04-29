cd175339f8745a6cadfa0a33781bc1bde4ef789e
yasu <hexapenta3@gmail.com>
2026-04-30 06:20:35 +0900

    commit

--- production/main.py
+++ production/main.py
@@ -6,7 +6,7 @@ def get_numbers():
     """
         1から10までの数字をlistで返す関数
     """
-    print('Add test comments 2.')
+    print('Add test comments 4.')
     print('func: get_numbers edit.')
     return list(range(1, 11))
 
